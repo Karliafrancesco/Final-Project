cdef9eb39fe50afd6d8283d4d3d12441f103cef1
Karl <karliafrancesco@gmail.com>
2022-08-04 10:21:05 -0400

    Created Header/NavBar

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+import { BiCameraMovie } from "react-icons/bi";
+import { NavLink, useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+   let nav = useNavigate();
+
+   return (
+      <>
+         <Container
+            onClick={() => {
+               nav("/");
+            }}
+         >
+            <Wrapper>
+               <TitleAndIcon>
+                  <BiCameraMovie style={{ color: "black" }} />
+                  <Title>Find Film</Title>
+               </TitleAndIcon>
+               <SignIn>Sign In</SignIn>
+            </Wrapper>
+         </Container>
+      </>
+   );
+};
+
+export default Navbar;
+
+const Wrapper = styled.div`
+   /* box-shadow: 0 6px 6px -6px gray; */
+   display: flex;
+   justify-content: space-between;
+   width: 100%;
+   position: relative;
+   background: #2b2b2b;
+`;
+
+const TitleAndIcon = styled.div`
+   display: flex;
+   align-items: center;
+   gap: 10px;
+   background-color: gold;
+   margin: 10px 0 10px 100px;
+   border-radius: 5px;
+   padding: 0 6px;
+`;
+
+const Container = styled.div`
+   /* position: fixed; */
+   width: 100%;
+   cursor: pointer;
+`;
+
+const Title = styled.div`
+   /* display: flex;
+   justify-content: center; */
+   font-size: 20px;
+   margin: 10px 0;
+   color: black;
+   font-weight: bold;
+   margin-left: 0px;
+`;
+
+const SignIn = styled.button`
+   background: transparent;
+   border: none;
+   margin-right: 100px;
+   color: white;
+   font-weight: bold;
+   cursor: pointer;
+`;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+   let nav = useNavigate();
+
+   return (
+      <Wrapper>
+         <NavBar>
+            <Options
+               onClick={() => {
+                  nav("/");
+               }}
+            >
+               Search Movie
+            </Options>
+            <Options>Search Actors</Options>
+            {/* <Options>Search vehicle</Options> */}
+         </NavBar>
+      </Wrapper>
+   );
+};
+
+export default Navbar;
+
+const Wrapper = styled.div`
+   display: flex;
+   justify-content: center;
+   color: white;
+   box-shadow: 0 6px 6px -6px gray;
+   font-weight: bold;
+   /* background: var(--color-background-blue); */
+   background: #232324;
+   box-shadow: 1px 6px 6px -6px gray;
+`;
+
+const NavBar = styled.div`
+   display: flex;
+   justify-content: center;
+   width: 100%;
+`;
+
+const Options = styled.button`
+   background-color: transparent;
+   font-size: 15px;
+   border: none;
+   display: flex;
+   text-align: center;
+   padding: 10px 20px;
+   border-bottom: 1px solid grey;
+   cursor: pointer;
+   color: white;
+   opacity: 0.5;
+
+   &:hover {
+      color: white;
+      opacity: 1;
+      /* border-bottom: 2px solid white; */
+   }
+
+   &.active {
+      color: blue;
+      border-radius: 35px;
+   }
+`;
